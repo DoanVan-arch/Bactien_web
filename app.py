5d6caaa5f0888c61c7703898c4106a477faae4ff
DoanVan-arch <skypro311294@gmail.com>
2026-07-08 09:19:29 +0700

    modified:   app.py

--- app.py
+++ app.py
@@ -226,4 +226,4 @@ if __name__ == '__main__':
         db.create_all()
         create_admin()
     init_cache()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)  # Enable threaded mode for handling multiple requests
